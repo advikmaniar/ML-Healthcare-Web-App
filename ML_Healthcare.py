8d85bef6496337f09eba71e6f9facd5a9db69c18
Advik Maniar <72503778+advikmaniar@users.noreply.github.com>
2021-06-24 18:43:32 +0530

    Update ML_Healthcare.py

--- ML_Healthcare.py
+++ ML_Healthcare.py
@@ -37,8 +37,7 @@ classifier_name = st.sidebar.selectbox("Select Classifier: ",("Logistic Regressi
 LE=LabelEncoder()
 def get_dataset(dataset_name):
     if dataset_name=="Heart Attack":
-        data=pd.read_csv("https://github.com/advikmaniar/ML-Heathcare-Web-App/blob/main/Data/heart.csv",names=['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh',
-       'exng', 'oldpeak', 'slp', 'caa', 'thall', 'output'])
+        data=pd.read_csv("https://github.com/advikmaniar/ML-Heathcare-Web-App/blob/main/Data/heart.csv")
         st.header("Heart Attack Prediction")
         return data
 
